80b1deaa6597fa7b3b5334024178c9cb6b4f304d
Allen <280894830@qq.com>
2018-03-19 21:03:39 +0800

    WebDapper

---
+++ src/Framework/Sherlock.Framework.Web.Dapper/DapperIdentityStarter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Sherlock.Framework.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Sherlock.Framework.Domain;
+using Sherlock.Framework;
+using Sherlock.Framework.Services;
+
+namespace Sherlock.Framework.Web
+{
+    public class DapperIdentityStarter<TUser, TRole, TIdentityService> : WebStarter
+        where TUser : class, IUser
+        where TRole : class, IRole
+        where TIdentityService : IIdentityService
+    {
+
+        private static void SetupIdentityOptions(IdentityOptions op, SherlockWebOptions options)
+        {
+            op.Password.RequireLowercase = false;
+            op.Password.RequireDigit = false;
+            op.Password.RequireNonAlphanumeric = false;
+            op.Password.RequireUppercase = false;
+            op.Password.RequireDigit = false;
+
+            op.SignIn.RequireConfirmedEmail = false;
+            op.SignIn.RequireConfirmedPhoneNumber = false;
+
+            op.Cookies.ApplicationCookie.LoginPath = options.LoginPath;
+            op.Cookies.ApplicationCookie.LogoutPath = options.LogoutPath;
+
+            op.Cookies.ApplicationCookie.CookieSecure = options.CookieSecure;
+            op.Cookies.ExternalCookie.CookieSecure = options.CookieSecure;
+            op.Cookies.TwoFactorRememberMeCookie.CookieSecure = options.CookieSecure;
+            op.Cookies.TwoFactorUserIdCookie.CookieSecure = options.CookieSecure;
+        }
+
+        public override void ConfigureServices(SherlockServicesBuilder servicesBuilder, SherlockWebOptions options)
+        {
+            var identitySvcdescriptor = ServiceDescriber.Scoped<IIdentityService, TIdentityService>();
+            servicesBuilder.ServiceCollection.AddSmart(identitySvcdescriptor);
+
+            servicesBuilder.ServiceCollection
+                        .AddIdentity<TUser, TRole>(iop => SetupIdentityOptions(iop, options))
+                        .AddDefaultTokenProviders()
+                        .AddDapperStores<TUser, TRole>();
+        }
+
+        public override void Start(IApplicationBuilder appBuilder, SherlockWebOptions options)
+        {
+            appBuilder.UseIdentity();
+        }
+    }
+}
---
+++ src/Framework/Sherlock.Framework.Web.Dapper/DependencyInjection/SchubertDapperExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Sherlock.Framework.DependencyInjection;
+using Sherlock.Framework.Web.AspNetIdentity;
+using Sherlock.Framework.Domain;
+using Sherlock.Framework.Web.DependencyInjection;
+using Sherlock.Framework.Web;
+using Sherlock.Framework.Services;
+
+namespace Sherlock
+{
+    public static class SherlockDapperExtensions
+    {
+        /// <summary>
+        /// 加入 Asp.Net Identity ，同时使用 Dapper 持久化存储。
+        /// </summary>
+        /// <typeparam name="TUser">用户类型。</typeparam>
+        /// <typeparam name="TRole">角色类型。</typeparam>
+        /// <typeparam name="TIdentityService"><see cref="IIdentityService"/></typeparam>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static SherlockWebBuilder AddIdentityWithDapperStores<TUser, TRole, TIdentityService>(this SherlockWebBuilder builder)
+            where TUser : class, IUser
+            where TRole : class, IRole
+            where TIdentityService : IIdentityService
+        {
+            builder.AddStarter(new DapperIdentityStarter<TUser, TRole, TIdentityService>());
+            return builder;
+        }
+
+        /// <summary>
+        /// 为 Asp.Net Identity 配置 Dapper 持久化存储。
+        /// </summary>
+        /// <typeparam name="TUser">用户类型。</typeparam>
+        /// <typeparam name="TRole">角色类型。</typeparam>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        internal static IdentityBuilder AddDapperStores<TUser, TRole>(this IdentityBuilder builder)
+            where TUser : class, IUser
+            where TRole : class, IRole
+        {
+            builder.Services.AddSmart(ServiceDescriber.Scoped<IUserStore<TUser>, DapperUserStore<TUser, TRole>>(SmartOptions.Replace));
+            builder.Services.AddSmart(ServiceDescriber.Scoped<IRoleStore<TRole>, DapperRoleStore<TRole>>(SmartOptions.Replace));
+
+            return builder;
+
+        }
+    }
+}
